0876ea45fe2902da2fa07adbd08195a3839ebfe7
Colin Woodbury <colingw@gmail.com>
2017-07-24 15:45:33 -0700

    Update to match recent VectorPipe

--- build.sbt
+++ build.sbt
@@ -2,14 +2,20 @@ name := """vp-io-test"""
 
 version := "1.0.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.11.11"
+
+scalacOptions := Seq(
+  "-Ypartial-unification"
+)
 
 libraryDependencies ++= Seq(
-  "org.apache.spark"            %% "spark-core"                  % "2.1.0",
-  "org.locationtech.geotrellis" %% "geotrellis-spark"            % "1.1.0-SNAPSHOT",
-  "org.locationtech.geotrellis" %% "geotrellis-s3"               % "1.1.0-SNAPSHOT",
-  "org.locationtech.geotrellis" %% "geotrellis-vector"           % "1.1.0-SNAPSHOT",
-  "org.locationtech.geotrellis" %% "geotrellis-vectortile"       % "1.1.0-SNAPSHOT",
-  "org.locationtech.geotrellis" %% "geotrellis-vectortile-spark" % "1.1.0-SNAPSHOT",
-  "com.azavea"                  %% "vectorpipe"                  % "1.0.0"
+  "org.apache.spark"            %% "spark-core"                  % "2.2.0",
+  "org.apache.spark"            %% "spark-hive"                  % "2.2.0",
+  "org.locationtech.geotrellis" %% "geotrellis-spark"            % "1.2.0-SNAPSHOT",
+  "org.locationtech.geotrellis" %% "geotrellis-s3"               % "1.2.0-SNAPSHOT",
+  "org.locationtech.geotrellis" %% "geotrellis-vector"           % "1.2.0-SNAPSHOT",
+  "org.locationtech.geotrellis" %% "geotrellis-vectortile"       % "1.2.0-SNAPSHOT",
+  "com.azavea"                  %% "vectorpipe"                  % "1.0.0",
+  "org.apache.hadoop"           %  "hadoop-aws"                  % "2.8.1",
+  "org.typelevel"               %% "cats"                        % "0.9.0"
 )
--- src/main/scala/vpio/IO.scala
+++ src/main/scala/vpio/IO.scala
@@ -1,61 +1,59 @@
 package vpio
 
 import geotrellis.proj4.{LatLng, WebMercator}
-import geotrellis.raster.DoubleCellType
 import geotrellis.spark._
 import geotrellis.spark.io._
-import geotrellis.spark.io.file._
 import geotrellis.spark.io.index.ZCurveKeyIndexMethod
+import geotrellis.spark.io.s3.{S3AttributeStore, S3LayerWriter}
 import geotrellis.spark.tiling._
 import geotrellis.vectortile.VectorTile
-import geotrellis.vectortile.spark._
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
 import vectorpipe._
+import vectorpipe.util.LayerMetadata
 
 // --- //
 
 object IO extends App {
   override def main(args: Array[String]): Unit = {
-    implicit val sc: SparkContext = new SparkContext(
-      new SparkConf()
-        .setMaster("local[*]")
-        .setAppName("vp-simplify")
-    )
+
+    implicit val ss: SparkSession = SparkSession.builder
+      .master("local[*]")
+      .appName("vp-orc-io")
+      .enableHiveSupport
+      .getOrCreate
 
     /* Silence the damn INFO logger */
     Logger.getRootLogger().setLevel(Level.ERROR)
 
-    /* Path to a OSM XML file */
-    val path: String = "data/north-van.osm"
+    /* Necessary for reading ORC files off S3 */
+    useS3(ss)
+
+    val source: String = "vancouver"
+
+    /* Path to an OSM ORC file */
+    val path: String = s"s3://vectortiles/orc/${source}.orc"
 
     /* For writing a compressed Tile Layer */
-    val catalog: String = "/home/colin/tiles/"
-    val store = FileAttributeStore(catalog)
-    val writer = FileLayerWriter(store)
-
-    /*
-    val catalog: SpatialKey => String = SaveToHadoop.spatialKeyToPath(
-      LayerId("north-van", 15),
-      "/home/colin/tiles/{name}/{z}/{x}/{y}.mvt"
-    )
-     */
+    val store = S3AttributeStore("vectortiles", "orc-catalog")
+    val writer = S3LayerWriter(store)
 
     val layout: LayoutDefinition =
-      ZoomedLayoutScheme.layoutForZoom(15, WebMercator.worldExtent, 512)
+      ZoomedLayoutScheme.layoutForZoom(14, WebMercator.worldExtent, 512)
 
-    osm.fromLocalXML(path) match {
+    osm.fromORC(path) match {
       case Left(e) => println(e)
       case Right((ns,ws,rs)) => {
 
-        /* Assumes that OSM XML is in LatLng */
+        /* Assumes that OSM ORC is in LatLng */
         val latlngFeats: RDD[osm.OSMFeature] =
-          osm.toFeatures(ns, ws, rs)
+          osm.toFeatures(ns.repartition(100), ws.repartition(10), rs)
 
         /* Reproject into WebMercator, the default for VTs */
         val wmFeats: RDD[osm.OSMFeature] =
-          latlngFeats.map(_.reproject(LatLng, WebMercator))
+          latlngFeats.repartition(100).map(_.reproject(LatLng, WebMercator))
 
         /* Associated each Feature with a SpatialKey */
         val fgrid: RDD[(SpatialKey, Iterable[osm.OSMFeature])] =
@@ -71,16 +69,10 @@ object IO extends App {
         /* Construct metadata for the Layer */
         val meta = LayerMetadata(layout, bounds)
 
-        println(s"TOTAL TILES: ${tiles.count}")
-
-        writer.write(LayerId("north-van", 15), ContextRDD(tiles, meta), ZCurveKeyIndexMethod)
-
-        tiles.take(3).foreach({ case (_,v) => println(v.pretty) })
-
-//        tiles.saveToHadoop(catalog)({ (k,v) => v.toBytes })
+        writer.write(LayerId(source, 14), ContextRDD(tiles, meta), ZCurveKeyIndexMethod)
       }
     }
 
-    sc.stop()
+    ss.stop()
   }
 }
